ebe54b1c70194ea57814c0057ddc19888b8c9445
elAnibals <raas835@gmail.com>
2023-01-20 01:34:28 -0600

    theme buffer line

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,49 +1,38 @@
-local present, bufferline = pcall(require, "bufferline")
+require("bufferline").setup{
 
-if not present then
-   return
-end
+options = {
+  -- indicator={
+  --   style = 'underline'
+  -- },
+  offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
+  buffer_close_icon = "",
+  modified_icon = "",
+  close_icon = "",
+  show_buffer_close_icons = false,
+  show_close_icon = false,
+  show_buffer_icons = true,
+  left_trunc_marker = "",
+  right_trunc_marker = "",
+  max_name_length = 30,
+  max_prefix_length = 30,
+  tab_size = 21,
+  show_tab_indicators = true,
+  enforce_regular_tabs = false,
+  view = "multiwindow",
+  separator_style = "thin",
+  always_show_bufferline = true,
+  diagnostics = "nvim_lsp",
+  diagnostics_update_in_insert = true,
+  -- diagnostics_indicator = function(count, level, diagnostics_dict, context)
+  --   return "(" .. count .. ")"
+  -- end,
+  themable = true,
 
-local options = {
-   options = {
-      offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
-      buffer_close_icon = "",
-      modified_icon = "",
-      close_icon = "",
-      show_close_icon = true,
-      left_trunc_marker = "",
-      right_trunc_marker = "",
-      max_name_length = 14,
-      max_prefix_length = 13,
-      tab_size = 20,
-      show_tab_indicators = true,
-      enforce_regular_tabs = false,
-      view = "multiwindow",
-      show_buffer_close_icons = true,
-      separator_style = "thin",
-      always_show_bufferline = true,
-      diagnostics = true,
-      themable = true,
-      custom_filter = function(buf_number)
-         -- Func to filter out our managed/persistent split terms
-         local present_type, type = pcall(function()
-            return vim.api.nvim_buf_get_var(buf_number, "term_type")
-         end)
-
-         if present_type then
-            if type == "vert" then
-               return false
-            elseif type == "hori" then
-               return false
-            end
-            return true
-         end
-
-         return true
-      end,
-   },
+  indicator_selected = {
+      fg = { attribute = "fg", highlight = "LspDiagnosticsDefaultHint" },
+      bg = { attribute = "bg", highlight = "Normal" },
+      bold = true,
+      italic = true
+    },
+  }
 }
-
--- check for any override
-
-bufferline.setup()
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -23,7 +23,6 @@ require'nvim-treesitter.configs'.setup {
 
   highlight = {
     enable = true,
-    use_languagetree = true,
     additional_vim_regex_highlighting = true,
     use_languagetree = true,
   },
